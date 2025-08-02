d3badadb2c3d4a40ab32277212efd92291dfe27a
rajeshchowdary684 <rajabhaiyt11@gmail.com>
2025-08-02 10:30:44 +0530

    Create Arithematic.java

---
+++ Arithematic.java
@@ -0,0 +1,14 @@
+package example1;
+
+
+public class Arithematic {
+
+  public static void main(String[] args) {
+      System.out.println(5-2); //3
+      System.out.println(5*2); //10
+      System.out.println(5/2); //2
+      System.out.println(5%2); //1
+
+  }
+
+}
